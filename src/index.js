9865e2cd2a70f467e8af0d4977e5fc26c45f7e4b
caandoll <caandoll@aliyun.com>
2019-03-03 18:52:48 +0800

    feat: 新增xlsx与xls支持

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const path = require('path');
 const xlsx = require('node-xlsx');
-// const ejsExcel = require('ejsexcel');
 const csvToJson = require('csvtojson');
 const root = process.cwd();
 const iconv = require('iconv-lite');
@@ -22,69 +21,73 @@ fs.readdir(runDir, (err, files) => {
   if (!fs.existsSync(generateDir)) {
     fs.mkdirSync(generateDir);
   }
+
   for (const file of files) {
-    if (file.indexOf('.csv') === -1) { // 不是csv文件
+    if (!/\.csv/.test(file) && !/\.xls(x)?/.test(file)) {
       continue;
     }
     const noRepeatArr = []; // 保存去重数组
     const filePath = path.join(runDir, file);
-    const stream = fs.createReadStream(filePath, { encoding: 'binary' });
-    let data = '';
-    stream.on('error', err => {
-      console.error('读取行错误');
-      console.error(err);
-    });
-    stream.on('data', chunk => {
-      data += chunk;
-    });
-    stream.on('end', () => {
-      // 重新编码，否则乱码
-      const buf = Buffer.from(data, 'binary');
-      const str = iconv.decode(buf, 'GBK');
-      csvToJson({
-        noheader: true,
-        output: 'csv',
-      }).fromString(str)
-        .subscribe((csvRow) => {
-          if (!noRepeatArr.find(item => item[ 1 ] === csvRow[ 1 ]) && /^http/.test(csvRow[ 4 ])) { // 名称去重 以及 去掉第五列不以http开头的网址
-            noRepeatArr.push(csvRow);
-          }
-        }, err => {
-          console.error('读取行错误');
+    // 过滤操作
+    const noRepeat = row => {
+      if (row && !noRepeatArr.find(item => item[ 1 ] === row[ 1 ]) && /^http/.test(row[ 4 ])) { // 名称去重 以及 去掉第五列不以http开头的网址
+        noRepeatArr.push(row);
+      }
+    };
+    // 生成文件函数，传入过滤后的源文件
+    const generator = noRepeatArr => {
+      // 随机排序
+      const newArr = noRepeatArr.sort(() => Math.random() - 0.5);
+      // 生成文件
+      const data = [
+        [ 'query', '改进现场', 'url', '打分', '备注' ],
+      ];
+      for (const item of newArr) {
+        data.push([ item[ 1 ], '=HYPERLINK(C2,"现场链接")', item[ 4 ] ]);
+      }
+      const buffer = xlsx.build([ { name: "sheet1", data } ]);
+      const newFile = file.replace('日', '日复评—').replace('原表', '').replace(/\.(csv|xls(x)?)$/, '.xlsx');
+      fs.writeFile(path.join(generateDir, newFile), buffer, 'utf8', () => {
+        if (err) {
+          console.error('写入错误');
           console.error(err);
-        }, () => {
-          // 随机排序
-          const newArr = noRepeatArr.sort(() => Math.random() - 0.5);
-          // 生成文件
-          const data = [
-            [ 'query', '改进现场', 'url', '打分', '备注' ],
-          ];
-          for (const item of newArr) {
-            data.push([ item[ 1 ], '=HYPERLINK(C2,"现场链接")', item[ 4 ] ]);
-          }
-          const buffer = xlsx.build([ { name: "sheet1", data } ]);
-          const newFile = file.replace('日', '日复评—').replace('原表.csv', '.xlsx');
-          fs.writeFile(path.join(generateDir, newFile), buffer, 'utf8', () => {
-            if (err) {
-              console.error('写入错误');
-              console.error(err);
-            }
-          })
-          /* fs.readFile(path.join(__dirname, 'template.xlsx'), async (err,exlBuf) => {
-            if (err) {
-              console.error('读取模板错误');
-              console.error(err);
-            }
-            const buffer = await ejsExcel.renderExcel(exlBuf, newArr);
-            const newFile = file.replace('日', '日复评—').replace('.csv', '.xlsx');
-            fs.writeFile(path.join(generateDir, newFile), buffer, 'utf8', () => {
-              if (err) {
-                console.error('写入错误');
-                console.error(err);
-              }
-            })
-          });*/
-        });
-    });
+        }
+      })
+    };
+    if (/\.csv/.test(file)) {
+      const stream = fs.createReadStream(filePath, { encoding: 'binary' });
+      let data = '';
+      stream.on('error', err => {
+        console.error('读取行错误');
+        console.error(err);
+      });
+      stream.on('data', chunk => {
+        data += chunk;
+      });
+      stream.on('end', () => {
+        // 重新编码，否则乱码
+        const buf = Buffer.from(data, 'binary');
+        const str = iconv.decode(buf, 'GBK');
+        csvToJson({
+          noheader: true,
+          output: 'csv',
+        }).fromString(str)
+          .subscribe((csvRow) => {
+            console.log(csvRow);
+            noRepeat(csvRow);
+          }, err => {
+            console.error('读取行错误');
+            console.error(err);
+          }, () => {
+            generator(noRepeatArr);
+          });
+      });
+    } else {
+      const sheet = xlsx.parse(filePath)[ 0 ].data;
+      for (let i = 0; i <= sheet.length; i++) {
+        noRepeat(sheet[ i ]);
+      }
+      generator(noRepeatArr);
+    }
   }
 });
